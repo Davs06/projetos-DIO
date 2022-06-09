85b3ad14391799a074d8c21bc72cdab6b714faaf
Davs06 <santosdv40@gmail.com>
2022-06-08 22:56:54 -0300

    primeiro contatto com js e internet

---
+++ js/js-cm-html/primeiro-contato/main.js
@@ -0,0 +1,9 @@
+var nome = "Davs "
+var idade = 23
+var idade2 = 10
+var frase = "são paulo é o melhor time do mundo"
+// alert ("Hello world \nBem vindo " + nome + idade + " anos");
+// alert (idade + idade2)
+
+console.log(nome + idade)
+console.log (frase.replace("são paulo", "santos")) // '.replace' substitui palavras 
